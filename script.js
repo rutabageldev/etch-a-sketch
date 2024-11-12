1f26b58ec3fbe83f0971da59d98340f5cf9db828
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2024-11-12 06:48:18 -0500

    Built index.html, styled interface, began scripting

---
+++ script.js
@@ -0,0 +1,11 @@
+/* 
+function createGrid(width, height)
+    squareWidth = gridWidth / width
+    squareHeight = gridHeight / height
+    createSquares(height * width) => create DOM elements
+*/
+
+function createGrid(width, height) {
+    let totalSquares = width * height;
+    
+}
